e9a236bb93923d81b00e098b8ab4957f24b39847
alonlevy5 <alonlevy5@gmail.com>
2022-01-13 18:31:04 +0200

    Update weather data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forcast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=83fc4fee614237f85d8337fbb869e9c0&query='+ latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=83fc4fee614237f85d8337fbb869e9c0&query='+ latitude + ',' + longitude 
 
     request({ url, json:true }, (error, {body}) => {
         if(error){
@@ -9,7 +9,8 @@ const forcast = (latitude, longitude, callback) => {
         } else if (body.error){
             callback('Unable to find location', undefined)
         } else{
-            callback(undefined, body.current.temperature)
+            callback(undefined,'The current humidity is ' + body.current.humidity +
+            ' The current temprature is ' + body.current.temperature + ' And it feels like ' + body.current.feelslike)
         }
     })
 
